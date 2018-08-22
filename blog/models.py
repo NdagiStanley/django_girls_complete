78457a0d264c4a50ead021d523a77599e591a42a
NdagiStanley <ndagis@gmail.com>
2018-08-22 12:38:06 +0300

    Add Views, Templates, Forms

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    """
+    model Post
+    """
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
--- mysite/settings.py
+++ mysite/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'de-ch'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Berlin'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
